309d3e34d2e2bd5113040aec1e4e9a1e21d5017c
Patryk Stępień <droznyk@gmail.com>
2018-02-23 10:52:30 +0100

    Add index and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+  root 'static_pages#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/contact', to: 'static_pages#contact'
 end
