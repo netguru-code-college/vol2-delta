a9bfab8e1053e803d27cb54859585af20ffd0116
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-02-23 15:24:32 +0100

    Sector partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  User.create(email: Faker::Internet.email,
+              password: 'password',
+              password_confirmation: 'password')
+end
+
+puts '10 Users created!'
+
+10.times do
+  Crag.create(name: Faker::Address.city,
+              country: Faker::Address.country,
+              x: rand,
+              y: rand
+            )
+end
+puts '10 Crags created!'
+
+10.times do
+  Sector.create(
+    name: Faker::BossaNova.song,
+    aspect: Faker::Cat.breed,
+    crag_id: rand(10)
+  )
+end
+puts '10 sectors created'
