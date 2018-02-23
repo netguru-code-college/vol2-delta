fc307ee522c76865a1ce0920c17b1b3699574666
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-02-23 14:12:44 +0100

    Create controller for sectors

---
+++ app/controllers/sectors_controller.rb
@@ -0,0 +1,42 @@
+class SectorsController < ApplicationController
+  before_action :set_sector, only: [:show, :edit, :destroy]
+
+  def index
+    @sectors = Sector.all
+  end
+
+  def show
+  end
+
+  def new
+    @sector = Sector.new(sector_params)
+  end
+
+  def create
+    if @sector.save
+      redirect root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @sector.destroy
+      redirect
+  end
+  private
+
+  def set_sector
+      @sector = Sector.find(params[:id])
+  end
+
+  def sector_params
+    params.require(:sector).permit(:name, :aspect, :x, :y, :crag_id)
+  end
+end
