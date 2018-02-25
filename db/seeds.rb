f5f36c4c4d84f27dc7390a8c45550fabf8fe37d5
Marcin G <marcinger@protonmail.com>
2018-02-25 11:10:34 +0100

    populates db with some crag data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Crag.create name: "Rudawy Janowickie", country: "Poland", latitude: 50.8083, longitude: 15.8950
+Crag.create name: "Sokoliki", country: "Poland", latitude: 50.8699, longitude: 15.8665
+Crag.create name: "Jura", country: "Poland", latitude: 50.206667, longitude: 19.829167
