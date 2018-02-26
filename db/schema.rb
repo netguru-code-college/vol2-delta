0eb2d0dce5d16e2451f009ddd72fce4e5dc70958
Marcin G <marcinger@protonmail.com>
2018-02-26 14:12:25 +0100

    moves <script> from partial to <head> - fixes gmaps problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180225101338) do
 
   # These are extensions that must be enabled in order to support this database
@@ -32,8 +31,8 @@ ActiveRecord::Schema.define(version: 20180225101338) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.bigint "climbing_route_id"
-    t.index ["climbing_route_id"], name: "index_ascents_on_climbing_route_id"
+    t.bigint "climbing_routes_id"
+    t.index ["climbing_routes_id"], name: "index_ascents_on_climbing_routes_id"
     t.index ["user_id"], name: "index_ascents_on_user_id"
   end
 
@@ -91,7 +90,7 @@ ActiveRecord::Schema.define(version: 20180225101338) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "ascents", "climbing_routes"
+  add_foreign_key "ascents", "climbing_routes", column: "climbing_routes_id"
   add_foreign_key "ascents", "users"
   add_foreign_key "climbing_routes", "sectors"
   add_foreign_key "sectors", "crags"
