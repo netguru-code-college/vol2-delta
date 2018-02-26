422821fa0b49a997d89a4bc34700c52d9fcb290b
Patryk Stępień <droznyk@gmail.com>
2018-02-26 14:52:22 +0100

    Refactor climbing route validations

--- app/models/climbing_route.rb
+++ app/models/climbing_route.rb
@@ -1,8 +1,5 @@
 class ClimbingRoute < ApplicationRecord
   validates :name,  presence: true
-  validates :grade, inclusion: { in: RouteGrades,
-                    message: "%{value} is not a valid grade" },
-                    presence: true
   belongs_to :sector
   has_many :ascents
   has_one :route_grade
