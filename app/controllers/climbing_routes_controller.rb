76d66f9ffc92c805e0d7e053c70a9819a09eb2f2
Patryk Stępień <droznyk@gmail.com>
2018-02-26 13:52:37 +0100

    Create climbing routes controller

---
+++ app/controllers/climbing_routes_controller.rb
@@ -0,0 +1,60 @@
+class ClimbingRoutesController < ApplicationController
+  attr_accessor :name, :grade, :number_of_ascents
+  before_action :fetch_climbing_route, only: [:show, :update, :edit, :destroy]
+  def index
+    @climbing_routes = ClimbingRoute.all.sort
+  end
+
+  def new
+    @climbing_route = ClimbingRoute.new
+    @sectors = Sector.all
+  end
+
+  def create
+    @climbing_route = ClimbingRoute.new(climbing_route_params)
+
+    if @climbing_route.save
+      redirect_to crag_sector_climbing_route_path(
+        @climbing_route.sector.crag,
+        @climbing_route.sector,
+        @climbing_route
+      ), notice: 'Climbing route was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    @sectors = Sector.all
+  end
+
+  def update
+    if @climbing_route.update(climbing_route_params)
+      redirect_to crag_sector_climbing_route_path(
+        @climbing_route.sector.crag,
+        @climbing_route.sector,
+        @climbing_route
+      ), notice: 'Climbing route was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @climbing_route.destroy
+    redirect_to crag_sector_climbing_routes_path, notice: 'Climbing route was successfully destroyed.'
+  end
+
+  private
+
+  def fetch_climbing_route
+    @climbing_route = ClimbingRoute.find(params[:id]).decorate
+  end
+
+  def climbing_route_params
+      params.require(:climbing_route).permit(:name, :grade, :number_of_ascents, :sector_id)
+  end
+end
