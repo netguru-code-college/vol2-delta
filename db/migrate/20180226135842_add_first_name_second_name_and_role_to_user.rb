024229c1e9193b464589f2e1fcca4d40702025a4
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-02-26 15:56:16 +0100

    Add cancancan

---
+++ db/migrate/20180226135842_add_first_name_second_name_and_role_to_user.rb
@@ -0,0 +1,7 @@
+class AddFirstNameSecondNameAndRoleToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :role, :string, default: 'regular'
+  end
+end
