b10e26ba0cec24582373a3685f32fe27f9b9497f
Karolina <karosk9@gmail.com>
2018-02-26 09:55:24 +0100

    seed database: points for grades and styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+AscentStyle.create(style: 'RP', points: 0)
+AscentStyle.create(style: 'OS', points: 150)
+AscentStyle.create(style: 'FL', points: 50)
+
+puts '3 ascent style created'
+
+RouteGrade.create(grade: '6a', points: 100)
+RouteGrade.create(grade: '6a+', points: 150)
+RouteGrade.create(grade: '6b', points: 200)
+RouteGrade.create(grade: '6b+', points: 250)
+RouteGrade.create(grade: '6c', points: 300)
+RouteGrade.create(grade: '6c+', points: 350)
+RouteGrade.create(grade: '7a', points: 400)
+RouteGrade.create(grade: '7a+', points: 450)
+RouteGrade.create(grade: '7b', points: 500)
+RouteGrade.create(grade: '7b+', points: 550)
+RouteGrade.create(grade: '7c', points: 600)
+RouteGrade.create(grade: '7c+', points: 650)
+RouteGrade.create(grade: '8a', points: 700)
+RouteGrade.create(grade: '8a+', points: 750)
+RouteGrade.create(grade: '8b', points: 800)
+RouteGrade.create(grade: '8b+', points: 850)
+RouteGrade.create(grade: '8c', points: 950)
+RouteGrade.create(grade: '8c+', points: 1000)
+RouteGrade.create(grade: '9a', points: 1050)
+RouteGrade.create(grade: '9a+', points: 1100)
+RouteGrade.create(grade: '9b', points: 1150)
+RouteGrade.create(grade: '9b+', points: 1200)
+RouteGrade.create(grade: '9c', points: 1250)
+
+puts 'grading system created'
