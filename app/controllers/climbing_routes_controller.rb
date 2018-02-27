1c54510bfb80ef19fcfd8a9e9eca34c838ed52e4
Patryk Stępień <droznyk@gmail.com>
2018-02-27 12:28:43 +0100

    handle in_crag_and_sector scope

--- app/controllers/climbing_routes_controller.rb
+++ app/controllers/climbing_routes_controller.rb
@@ -1,8 +1,10 @@
 class ClimbingRoutesController < ApplicationController
   attr_accessor :name, :grade, :number_of_ascents, :route_grade_id
-  before_action :fetch_climbing_route, only: [:show, :update, :edit, :destroy]
+  before_action :fetch_climbing_route, only: %i[show update edit destroy]
   def index
-    @climbing_routes = ClimbingRoute.includes(sector: :crag).all.sort
+    binding.pry
+    @climbing_routes = ClimbingRoute.includes(sector: :crag)
+                                    .in_crag_and_sector(params[:crag_id], params[:sector_id])
   end
 
   def new
@@ -11,7 +13,7 @@ class ClimbingRoutesController < ApplicationController
 
   def create
     @climbing_route = ClimbingRoute.new(climbing_route_params)
-
+    binding.pry
     if @climbing_route.save
       redirect_to crag_sector_climbing_route_path(
         @climbing_route.sector.crag,
@@ -23,11 +25,9 @@ class ClimbingRoutesController < ApplicationController
     end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @climbing_route.update(climbing_route_params)
@@ -53,6 +53,6 @@ class ClimbingRoutesController < ApplicationController
   end
 
   def climbing_route_params
-      params.require(:climbing_route).permit(:name, :grade, :number_of_ascents, :sector_id)
+    params.require(:climbing_route).permit(:name, :number_of_ascents, :sector_id, :route_grade_id)
   end
 end
--- app/models/climbing_route.rb
+++ app/models/climbing_route.rb
@@ -1,6 +1,16 @@
 class ClimbingRoute < ApplicationRecord
   validates :name,  presence: true
+
   belongs_to :sector
   has_many :ascents
   has_one :route_grade
+
+  scope :in_crag_and_sector,
+        lambda { |sector_id, crag_id|
+          joins(:sector)
+            .where(sector_id: sector_id)
+            .where('sectors.crag_id = ?', crag_id)
+        }
 end
+
+ClimbingRoute.includes(:sector).where(sector_id: 3).where('sector.crag_id = ?', 4)
