b63de1e49249529d5711fa65a9335105d45fc072
Marcin G <marcinger@protonmail.com>
2018-02-28 22:31:41 +0100

    duplicate in crag model

--- app/models/crag.rb
+++ app/models/crag.rb
@@ -1,7 +1,4 @@
 class Crag < ApplicationRecord
   validates :name,  presence: true
-  validates :country,  presence: true
-  validates :latitude,  presence: true
-  validates :longitude,  presence: true
   has_many :sectors, dependent: :destroy
 end
