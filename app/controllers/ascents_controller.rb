e7359c3e75dc8d2faa4e92488b47e5541cb35f93
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-02-28 14:21:34 +0100

    Fixed ascents controller

--- app/controllers/ascents_controller.rb
+++ app/controllers/ascents_controller.rb
@@ -39,20 +39,21 @@ def destroy
   redirect_to ascents_url, notice: 'Ascent was successfully deleted.'
 end
 
-private
-# Use callbacks to share common setup or constraints between actions.
-def set_ascent
-  @ascent = Ascent.find(params[:id])
-end
-# Only allow a trusted parameter "white list" through.
-def ascent_params
-  params.require(:ascent).permit(:comment, :date, :style)
-end
-# Find climbing route to add ascent.
-def climbing_route_params
-  @climbing_route = ClimbingRoute.eager_load(sector: :crag).find(params[:climbing_route_id])
-end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ascent
+    @ascent = Ascent.find(params[:id])
+  end
+  # Only allow a trusted parameter "white list" through.
+  def ascent_params
+    params.require(:ascent).permit(:comment, :date, :style)
+  end
+  # Find climbing route to add ascent.
+  def climbing_route_params
+    @climbing_route = ClimbingRoute.eager_load(sector: :crag).find(params[:climbing_route_id])
+  end
 
-def set_user
-  @user = current_user
+  def set_user
+    @user = current_user
+  end
 end
