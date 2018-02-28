a491a33c8eef9d0733550aa1cfbe59d0682d0571
Patryk Stępień <droznyk@gmail.com>
2018-02-28 09:36:21 +0100

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,28 +3,41 @@ Crag.create name: "Rudawy Janowickie", country: "Poland", latitude: 50.8083, lon
 Crag.create name: "Sokoliki", country: "Poland", latitude: 50.8699, longitude: 15.8665
 Crag.create name: "Jura", country: "Poland", latitude: 50.206667, longitude: 19.829167
 
-puts '3 crags created'
-
-10.times do
-  Sector.create(
-    name: Faker::BossaNova.song,
-    aspect: Faker::Cat.breed,
-    latitude: rand(1..100),
-    longitude: rand(1..100),
-    crag_id: (Crag.all.map(&:id)).sample
+7.times do
+  Crag.create(
+    name: Faker::Address.city,
+    country: Faker::Address.country,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude
   )
 end
-puts '10 sectors created'
+
+puts "#{Crag.all.count} crags created"
+
+Crag.all.each do |crag|
+  10.times do
+    Sector.create(
+      name: Faker::BossaNova.song,
+      aspect: Faker::Cat.breed,
+      latitude: Faker::Address.latitude,
+      longitude: Faker::Address.longitude,
+      crag_id: crag.id
+    )
+  end
+end
+puts "#{Sector.all.count} sectors created"
 
 
 
 10.times do
   User.create(email: Faker::Internet.email,
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
               password: 'password',
               password_confirmation: 'password')
 end
 
-puts '10 Users created!'
+puts "#{User.all.count} Users created!"
 
 AscentStyle.create(style: 'RP', points: 0)
 AscentStyle.create(style: 'OS', points: 150)
@@ -58,25 +71,30 @@ RouteGrade.create(grade: '9c', points: 1250)
 
 puts 'grading system created'
 
-10.times do
-  ClimbingRoute.create(
-    name: Faker::Beer.name,
-    number_of_ascents: 0,
-    sector_id: (Sector.all.map(&:id)).sample,
-    route_grade_id: (RouteGrade.all.map(&:id)).sample
-  )
+Sector.all.each do |sector|
+  5.times do
+    ClimbingRoute.create(
+      name: Faker::Beer.name,
+      number_of_ascents: 0,
+      sector_id: sector.id,
+      route_grade_id: (RouteGrade.all.map(&:id)).sample
+    )
+  end
 end
 
-puts '10 climbing routes created'
+puts "#{ClimbingRoute.all.count} climbing routes created"
 
-10.times do
-  Ascent.create(
-    comment: Faker::Beer.name,
-    date: rand(10).years.ago,
-    user_id: (User.all.map(&:id)).sample,
-    climbing_route_id: (ClimbingRoute.all.map(&:id)).sample,
-    ascent_style_id: (AscentStyle.all.map(&:id)).sample
-  )
+ClimbingRoute.all.each do |climbing_route|
+  5.times do
+    Ascent.create(
+      comment: Faker::Beer.name,
+      date: Faker::Date.between(2.years.ago, Date.today),
+      user_id: (User.all.map(&:id)).sample,
+      climbing_route_id: climbing_route.id,
+      ascent_style_id: (AscentStyle.all.map(&:id)).sample,
+    )
+    climbing_route.increment!(:number_of_ascents)
+  end
 end
 
-puts '10 ascents created'
+puts "#{Ascent.all.count} ascents created"
