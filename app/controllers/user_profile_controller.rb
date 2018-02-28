08308d93870cae2a24de6ea44ab4b12a790cc3a5
Marcin G <marcinger@protonmail.com>
2018-02-28 14:06:06 +0100

    adds pagination to user profile page

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,6 +1,6 @@
 class UserProfileController < ApplicationController
   def show
     @user = User.eager_load(:ascents).find(params[:id])
-
+    @ascents = @user.ascents.page(params[:page]).per(10)
   end
 end
