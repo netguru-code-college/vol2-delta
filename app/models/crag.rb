30f400f602f7230445db0966d0842c458c276052
Marcin G <marcinger@protonmail.com>
2018-02-28 22:26:54 +0100

    adds validations to crag model

--- app/models/crag.rb
+++ app/models/crag.rb
@@ -1,5 +1,7 @@
 class Crag < ApplicationRecord
   validates :name,  presence: true
-  validates :name,  presence: true
+  validates :country,  presence: true
+  validates :latitude,  presence: true
+  validates :longitude,  presence: true
   has_many :sectors, dependent: :destroy
 end
