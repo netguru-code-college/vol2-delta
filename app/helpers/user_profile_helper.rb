190021e544d5284652f584d788227cebbcb060f8
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-03-01 14:51:24 +0100

    fixed user helper

--- app/helpers/user_profile_helper.rb
+++ app/helpers/user_profile_helper.rb
@@ -5,7 +5,8 @@ module UserProfileHelper
 
   def full_user_name user
     user.first_name + " " + user.last_name
-
+  end
+  
   def user_full_name(user:)
     "#{user.first_name} #{user.last_name}"
   end
