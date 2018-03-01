53442c2093754891a9b8fc093aa12d407ed9c3c8
Marcin G <marcinger@protonmail.com>
2018-03-01 13:50:15 +0100

    changes email adress to users name in ranking

--- app/helpers/user_profile_helper.rb
+++ app/helpers/user_profile_helper.rb
@@ -2,4 +2,8 @@ module UserProfileHelper
   def find_ascent_grade route_grade_id
     RouteGrade.find(route_grade_id).grade
   end
+
+  def full_user_name user
+    user.first_name + " " + user.last_name
+  end
 end
