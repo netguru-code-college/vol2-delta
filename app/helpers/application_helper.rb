6f6df28215826265e2e735d8eb6eeb1125712193
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-03-03 17:31:52 +0100

    Working active navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,68 @@ module ApplicationHelper
   def body_id
     "#{controller.controller_name.dasherize}-#{controller.action_name.dasherize}"
   end
+
+  def navbar_helper
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        controller: 'static_pages',
+        action: 'index',
+        title: 'Home'
+      },
+      {
+        url: ranking_index_path,
+        controller: 'ranking',
+        title: 'Ranking'
+      },
+      {
+        url: crags_path,
+        controller: 'crags',
+        title: 'Crags',
+      },
+      {
+        url: climbing_routes_path,
+        controller: 'climbing_routes',
+        title: 'Climbing routes',
+      },
+      {
+        url: rules_path,
+        controller: 'static_pages',
+        action: 'rules',
+        title: 'Rules'
+      },
+      {
+        url: contact_path,
+        controller: 'static_pages',
+        action: 'contact',
+        title: 'Contact'
+      },
+      {
+         url: about_path,
+         controller: 'static_pages',
+         action: 'about',
+         title: 'About'
+       }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active?(item[:controller], item[:action])}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active?(controller, action)
+    if controller_name == controller && action_name == action
+      "active"
+    elsif controller_name != 'static_pages' && controller_name == controller
+      "active"
+    end
+  end
 end
