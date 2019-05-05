baa9811b4d6c9cd180b060d012fd386e0052e3bf
karolina oska <karolina.oska@netguru.pl>
2019-05-05 22:49:14 +0200

    Rearrange models structure to improve readability

--- app/models/ascent.rb
+++ app/models/ascent.rb
@@ -1,7 +1,8 @@
 class Ascent < ApplicationRecord
-  validates :comment, length: { maximum: 500 }
-  validates :date, presence: true
   belongs_to :user
   belongs_to :climbing_route
   belongs_to :ascent_style
+  
+  validates :comment, length: { maximum: 500 }
+  validates :date, presence: true
 end
