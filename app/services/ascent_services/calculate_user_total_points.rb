7ab19575f0f75d0a97c65c418fff35a8d3be68ec
karolina oska <karolina.oska@netguru.pl>
2019-05-05 22:52:14 +0200

    Refactor services

--- app/services/ascent_services/calculate_user_total_points.rb
+++ app/services/ascent_services/calculate_user_total_points.rb
@@ -1,18 +1,18 @@
 module AscentServices
-  class CalculateUserTotalPoints
-    def initialize(ascent:)
+  class CalculateUserTotalPoints < ApplicationService
+    def call(ascent:)
       @ascent = ascent
-    end
-
-    def call
+      
       calculate_user_total_points
     end
 
     private
 
+    attr_reader :ascent
+
     def calculate_user_total_points
-      @ascent.user.total_points += @ascent.points
-      @ascent.user.save
+      ascent.user.total_points += ascent.points
+      ascent.user.save
     end
   end
 end
